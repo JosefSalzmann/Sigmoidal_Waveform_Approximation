e6df881fef2d2116ed268d25f09f20061f32eb81
JosefSalzmann <josefsalzmann@hotmail.com>
2021-11-23 15:48:44 +0100

    Split unit tests into multiple files

---
+++ src/circuit_file_parser_tests.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include "circuit_file_parser.cpp"
+
+TEST(ParserTest, SanitiyCheck) {
+	CircuitFileParser Parser1 = CircuitFileParser();
+	Parser1.ParseFile("testing/multiple_inputs.txt");
+	ASSERT_EQ(false, Parser1.PerformSanityCheck());
+
+	CircuitFileParser Parser2 = CircuitFileParser();
+	Parser2.ParseFile("testing/mutliple_gates_same_output_node.txt");
+	ASSERT_EQ(false, Parser2.PerformSanityCheck());
+}
--- src/unit_tests.cpp
+++ src/unit_tests.cpp
@@ -1,17 +1,5 @@
 #include <gtest/gtest.h>
 
-#include "circuit_file_parser.cpp"
-
-TEST(ParserTest, SanitiyCheck) {
-	CircuitFileParser Parser1 = CircuitFileParser();
-	Parser1.ParseFile("testing/multiple_inputs.txt");
-	ASSERT_EQ(false, Parser1.PerformSanityCheck());
-
-	CircuitFileParser Parser2 = CircuitFileParser();
-	Parser2.ParseFile("testing/mutliple_gates_same_output_node.txt");
-	ASSERT_EQ(false, Parser2.PerformSanityCheck());
-}
-
 int main(int argc, char **argv) {
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
