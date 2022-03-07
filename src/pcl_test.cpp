d2c2716172994338efee25239f8df0937a407f21
JosefSalzmann <josefsalzmann@hotmail.com>
2022-03-07 20:42:19 +0100

    test convex hull calculation

--- src/pcl_test.cpp
+++ src/pcl_test.cpp
@@ -1,23 +1,41 @@
 #include "pcl_test.h"
 
+#include <pcl/ModelCoefficients.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/project_inliers.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/surface/convex_hull.h>
 
 #include <iostream>
 
 void PCLTest::test() {
-	pcl::PointCloud<pcl::PointXYZ> cloud;
-	cloud.width = 5;
-	cloud.height = 1;
-	cloud.is_dense = false;
-	cloud.resize(cloud.width * cloud.height);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	cloud->width = 60;
+	cloud->height = 1;
+	cloud->is_dense = false;
+	cloud->resize(cloud->width * cloud->height);
 
-	for (auto& point : cloud) {
+	for (auto& point : *cloud) {
 		point.x = 1024 * rand() / (RAND_MAX + 1.0f);
 		point.y = 1024 * rand() / (RAND_MAX + 1.0f);
 		point.z = 1024 * rand() / (RAND_MAX + 1.0f);
 	}
 	std::cout << "test" << std::endl;
-	for (const auto& point : cloud)
+	for (const auto& point : *cloud)
 		std::cout << "    " << point.x << " " << point.y << " " << point.z << std::endl;
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_hull(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::ConvexHull<pcl::PointXYZ> chull;
+	chull.setInputCloud(cloud);
+	chull.reconstruct(*cloud_hull);
+
+	std::cerr << "Convex hull has: " << cloud_hull->size()
+	          << " data points." << std::endl;
+
+	pcl::PCDWriter writer;
+	writer.write("table_scene_mug_stereo_textured_hull.pcd", *cloud_hull, false);
 }
