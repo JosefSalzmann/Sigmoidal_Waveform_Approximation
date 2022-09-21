43da6b1a53ce07ac076b926978185669646e601e
JosefSalzmann <josefsalzmann@hotmail.com>
2022-09-21 18:23:36 +0200

    add licence text to every source file

--- src/circuit_file_parser.cpp
+++ src/circuit_file_parser.cpp
@@ -1,6 +1,21 @@
 /*
-*   Parser for circuit files
-*/
+  File: cgal_test.h
+  Author: Josef Salzmann <josef.salzmann@tuwien.ac.at>
+  Copyright (c) 2022
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "circuit_file_parser.h"
 
@@ -109,7 +124,7 @@ void CircuitFileParser::SetPathToCircuitFile(const std::string& file_path) {
  * Nodes with more than one source cause abortion.
  * TODO: Maybe cause abortion when a sink has no source?
  * TODO: Check if all gate and input names are different
-*/
+ */
 bool CircuitFileParser::PerformSanityCheck() {
 	std::multiset<std::string> source_names, sink_names;
 
--- src/transition_schedule.h
+++ src/transition_schedule.h
@@ -1,6 +1,22 @@
 /*
- *   Transition Schedule class
+  File: transition_schedule.h
+  Author: Josef Salzmann <josef.salzmann@tuwien.ac.at>
+  Copyright (c) 2022
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef TRANSITION_SCHEDULE_H
 #define TRANSITION_SCHEDULE_H
 #include <memory>
